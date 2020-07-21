ba71d8e5728713af9ef1f2eadce7bd8b0c7cc838
Logic.liu <Logic.liu@hbc.inc>
2020-07-21 11:32:44 +0800

    init commit

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    主页
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
